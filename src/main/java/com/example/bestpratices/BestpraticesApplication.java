a6f26e37e0d4a1f9174bbbc5783f9d8e3d068307
Lucas <ldoliveira91@outlook.com>
2019-04-12 08:38:17 -0300

    project creation

---
+++ src/main/java/com/example/bestpratices/BestpraticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestpratices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestpraticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestpraticesApplication.class, args);
+	}
+
+}
